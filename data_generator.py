10ff8e21659731e60433f12bdde11233b4c44476
Tomas Surik <tomas.surik@gmail.com>
2024-03-03 20:56:26 +0000

    Removed model code from here.

--- data_generator.py
+++ data_generator.py
@@ -2,8 +2,6 @@ import numpy as np
 from statsmodels.stats.proportion import proportions_ztest
 from dataclasses import dataclass
 from scipy.stats import bernoulli
-import torch
-import torch.nn as nn
 
 @dataclass
 class DataGenerator:
@@ -30,18 +28,3 @@ class DataGenerator:
             is_significant.append(p_value < self.alpha)
 
         return (nobs_0, nobs_1, p_0, p_1), np.round(np.mean(is_significant), 4)
-
-class NeuralNetwork(nn.Module):
-    def __init__(self):
-        super(NeuralNetwork, self).__init__()
-        self.input_layer = nn.Linear(4, 10)   # 4 input neurons, 10 neurons in the first hidden layer
-        self.hidden_layer1 = nn.Linear(10, 8)  # 10 neurons in the first hidden layer, 8 neurons in the second hidden layer
-        self.hidden_layer2 = nn.Linear(8, 6)  # 8 neurons in the second hidden layer, 6 neurons in the third hidden layer
-        self.output_layer = nn.Linear(6, 1)  # 6 neurons in the third hidden layer, 1 output neuron
-
-    def forward(self, x):
-        x = torch.relu(self.input_layer(x))
-        x = torch.relu(self.hidden_layer1(x))
-        x = torch.relu(self.hidden_layer2(x))
-        x = torch.sigmoid(self.output_layer(x))
-        return x
